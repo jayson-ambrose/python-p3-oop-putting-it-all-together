eb6f92afdd7bdf215888fd91df195e799e259ea5
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-16 09:23:02 -0600

    lab completed

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,57 @@
 #!/usr/bin/env python3
 
+import ipdb
+
+# class Book:
+
+#     def __init__(self, title, page_count = 0):
+#         self.title = title
+#         self.page_count = page_count
+
+#     def turn_page(self):
+#         print("Flipping the page...wow, you read fast!")
+
+#     def get_page_count (self):
+#         return self._page_count
+
+#     def set_page_count (self, value):
+
+#         if (type(value) == int):
+#             print("tacos")
+#             self._page_count = value
+            
+#         print("page_count must be an integer")
+#         pass    
+
+#     page_count = property(get_page_count, set_page_count)
+
+
 class Book:
-    pass
+
+    def __init__(self, title, page_count = 0):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def get_page_count(self):
+        return  self._page_count
+    
+    def set_page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+        pass
+
+    page_count = property(get_page_count, set_page_count)
+
+    
+
+
+
+
+    
+
+
